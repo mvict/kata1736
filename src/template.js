9671fe3aa5461ff7309f7e2be8e007b2ef4256f3
mvict <mvictoria.avila@gmail.com>
2022-11-30 12:06:55 +0100

    build(MineSweeper): second pommodoro

--- src/template.js
+++ src/template.js
@@ -6,6 +6,26 @@ function isABomb(x) {
   return aBomb;
 }
 
+function showEmptyBoard(board, moveList) {
+  let emptyBoard = "";
+  if (moveList.length === 0) {
+    emptyBoard = `+-+-+-+
+| | | |
++-+-+-+
+| | | |
++-+-+-+
+| | | |
++-+-+-+`;
+  }
+  console.log(emptyBoard);
+  return emptyBoard;
+}
+
+function lookUpSquareInBoard(board, square) {
+  return board[square[0]][square[1]];
+}
 module.exports = {
   isABomb,
+  showEmptyBoard,
+  lookUpSquareInBoard,
 };
--- test/template.test.js
+++ test/template.test.js
@@ -1,10 +1,56 @@
-const { isABomb } = require("../src/template");
+const {
+  isABomb,
+  showEmptyBoard,
+  lookUpSquareInBoard,
+} = require("../src/template");
 
-describe("You have to clear a board, if step on a bomb you lose", () => {
+describe("You want to know if its a bomb or not", () => {
   it("A bomb is marked wit an X", () => {
     expect(isABomb("X")).toBe(true);
   });
   it("A space is not a bomb", () => {
     expect(isABomb(" ")).toBe(false);
   });
+});
+describe("You want to know what's in a square", () => {
+  const board = [
+    [" ", " ", " "],
+    [" ", "X", " "],
+    [" ", " ", " "],
+  ];
+  it("Return the value of a square with a bomb", () => {
+    expect(lookUpSquareInBoard(board, [1, 1])).toBe("X");
+  });
+  it("Return the value of a square with a bomb", () => {
+    expect(lookUpSquareInBoard(board, [0, 0])).toBe(" ");
+  });
+});
+
+describe("Player wants to follow the game on the terminal", () => {
+  it("At the beginning of the game an empty board is shown", () => {
+    const emptyBoard = `+-+-+-+
+| | | |
++-+-+-+
+| | | |
++-+-+-+
+| | | |
++-+-+-+`;
+    const board = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
+    expect(showEmptyBoard(board, [])).toBe(emptyBoard);
+  });
+  //   it("After one movement, one square will be filled", () => {
+  //     const board = `+-+-+-+
+  // | | | |
+  // +-+-+-+
+  // | |X| |
+  // +-+-+-+
+  // | | | |
+  // +-+-+-+`;
+  //     const board = [[" ", " ", " "], [" ", "X", " "], [" ", " ", " "]];
+  //     expect(showEmptyBoard(board, [1,1])).toBe(emptyBoard);
+  //   });
 });
